2f601e531c93ae6ce6552c8ea4f0125cad21e2ec
Jaku-BB <jaku.bb@outlook.com>
2023-11-24 02:31:18 +0100

    chore: reset migration history

--- prisma/migrations/20231124012612_initialization/migration.sql
+++ prisma/migrations/20231124012612_initialization/migration.sql
@@ -1,6 +1,7 @@
 -- CreateTable
 CREATE TABLE "cooldown" (
     "id" SERIAL NOT NULL,
+    "scope_key" TEXT,
     "interaction_key" TEXT NOT NULL,
     "user_id" TEXT NOT NULL,
     "expires_at" TIMESTAMP(3) NOT NULL,
